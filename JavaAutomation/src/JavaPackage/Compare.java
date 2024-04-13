e7bf11b09bf535fb4bf9209a6e8d83ef2cda23fc
Abinayavignesh <163851137+Abinayavignesh@users.noreply.github.com>
2024-04-13 13:59:17 +0530

    Update Compare.java

--- JavaAutomation/src/JavaPackage/Compare.java
+++ JavaAutomation/src/JavaPackage/Compare.java
@@ -3,10 +3,25 @@ package JavaPackage;
 public class Compare {
 
 	public static void main(String[] args) {
-		int a=5, b=10, c=3, d=4;
-		if(a+b>c+d) {
-			System.out.println("Sum of a+b is greater");
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the First number:");
+		int a=sc.nextInt();
+		System.out.println("Enter the Second number:");
+		int b=sc.nextInt();
+		System.out.println("Enter the Third number:");
+		int c=sc.nextInt();
+		System.out.println("Enter the Fourth number:");
+		int d=sc.nextInt();
+		int first=a+b;
+		int second=c+d;
+		if(first>second) {
+			System.out.println("Sum of a+b is greater"+ first);
 		}
+		else
+		{
+			System.out.println("Sum of c+d is greater"+ Second);
+		}
+
 
 	}
 
