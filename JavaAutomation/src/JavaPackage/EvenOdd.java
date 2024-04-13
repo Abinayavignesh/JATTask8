4c0d5ef0ec0b9e21f083826ad849b154c7bc3db2
Abinayavignesh <163851137+Abinayavignesh@users.noreply.github.com>
2024-04-13 14:03:27 +0530

    EvenOdd.java

--- JavaAutomation/src/JavaPackage/EvenOdd.java
+++ JavaAutomation/src/JavaPackage/EvenOdd.java
@@ -3,10 +3,13 @@ package JavaPackage;
 public class EvenOdd {
 
 	public static void main(String[] args) {
-		int value=4;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter a number:");
+		int value = scan.nextInt();
 		if(value%2==0) {
-			System.out.println("The number is a Even number" + value);
+			System.out.println("The number is a Even number :" + value);
 		}
+
 
 	}
 
